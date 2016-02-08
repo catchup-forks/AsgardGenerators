6e3ba7a2065d59aea83aa238ba4a6ce8365d0bf7
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-08 10:00:11 +0100

    Initialized required options and arguments for generator

--- Console/GenerateStructureCommand.php
+++ Console/GenerateStructureCommand.php
@@ -3,74 +3,111 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Illuminate\Config\Repository as Config;
 
-class GenerateStructureCommand extends Command {
+class GenerateStructureCommand extends Command
+{
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'bitsoflove:structure';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'bitsoflove:structure';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Command description.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    protected $config;
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
-		$config = app()->make('config');
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
 
-        var_dump($config->get('database.default'));
+        parent::__construct();
+    }
 
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return [
-			['tables', InputArgument::OPTIONAL, 'A list of Tables you wish to Generate Migrations for separated by a comma: users,posts,comments'],
-		];
-	}
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return [
-//		['connection', 'c', InputOption::VALUE_OPTIONAL, 'The database connection to use.', $this->config->get( 'database.default' )],
-//		['tables', 't', InputOption::VALUE_OPTIONAL, 'A list of Tables you wish to Generate Migrations for separated by a comma: users,posts,comments'],
-//		['ignore', 'i', InputOption::VALUE_OPTIONAL, 'A list of Tables you wish to ignore, separated by a comma: users,posts,comments' ],
-//		['path', 'p', InputOption::VALUE_OPTIONAL, 'Where should the file be created?'],
-//		['templatePath', 'tp', InputOption::VALUE_OPTIONAL, 'The location of the template for this generator'],
-//		['defaultIndexNames', null, InputOption::VALUE_NONE, 'Don\'t use db index names for migrations'],
-//		['defaultFKNames', null, InputOption::VALUE_NONE, 'Don\'t use db foreign key names for migrations'],
-		];
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+          [
+            'tables',
+            InputArgument::OPTIONAL,
+            'A list of Tables you wish to Generate Migrations for separated by a comma: users,posts,comments'
+          ],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+          [
+            'connection',
+            'c',
+            InputOption::VALUE_OPTIONAL,
+            'The database connection to use.',
+            $this->config->get('database.default')
+          ],
+          [
+            'ignore',
+            'i',
+            InputOption::VALUE_OPTIONAL,
+            'A list of Tables you wish to ignore, separated by a comma: users,posts,comments'
+          ],
+          [
+            'path',
+            'p',
+            InputOption::VALUE_OPTIONAL,
+            'Where should the file be created?'
+          ],
+          [
+            'templatePath',
+            'tp',
+            InputOption::VALUE_OPTIONAL,
+            'The location of the template for this generator'
+          ],
+          [
+            'defaultIndexNames',
+            null,
+            InputOption::VALUE_NONE,
+            'Don\'t use db index names for migrations'
+          ],
+          [
+            'defaultFKNames',
+            null,
+            InputOption::VALUE_NONE,
+            'Don\'t use db foreign key names for migrations'
+          ],
+        ];
+    }
 
 }
--- Providers/AsgardgeneratorsServiceProvider.php
+++ Providers/AsgardgeneratorsServiceProvider.php
@@ -5,6 +5,7 @@ use Modules\Asgardgenerators\Console\GenerateStructureCommand;
 
 class AsgardgeneratorsServiceProvider extends ServiceProvider
 {
+
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -19,44 +20,21 @@ class AsgardgeneratorsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerBindings();
-
-//        new GenerateStructureCommand()
-
         $this->registerGenerateStructureCommand();
 
         $this->commands([
-            'command.bitsoflove.structure'
+          'command.bitsoflove.structure'
         ]);
 
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array();
-    }
-
-    private function registerBindings()
-    {
-// add bindings
     }
 
     private function registerGenerateStructureCommand()
     {
 
-    $this->app->bindShared('command.bitsoflove.structure', function($app){
-        return new GenerateStructureCommand();
-    });
-
-
-//        $this->app->bindShared('command.asgard.module.update', function ($app) {
-//            return new UpdateModuleCommand(new Composer($app['files'], base_path()));
-//        });
-
+        $this->app->bindShared('command.bitsoflove.structure', function ($app) {
+            return new GenerateStructureCommand(
+              $app['config']
+            );
+        });
     }
 }
