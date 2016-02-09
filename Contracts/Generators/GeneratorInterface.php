85d5c53ba07aee7f18c53e8d4bc3ebc3f37cc9e1
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-09 10:20:09 +0100

    Added extra functions to interface

--- Contracts/Generators/GeneratorInterface.php
+++ Contracts/Generators/GeneratorInterface.php
@@ -2,10 +2,36 @@
 
 namespace Modules\Asgardgenerators\Contracts\Generators;
 
-interface GeneratorInterface {
+interface GeneratorInterface
+{
 
-
+    /**
+     * Execute the generator
+     *
+     * @return void
+     */
     public function execute();
+
+    /**
+     * Full path to the required template file
+     *
+     * @return string
+     */
+    public function getTemplatePath();
+
+    /**
+     * Create the data used in the template file
+     *
+     * @return array
+     */
+    public function getTemplateData();
+
+    /**
+     * Full path to the output file
+     *
+     * @return string
+     */
+    public function getFileGenerationPath();
 
 
 }
