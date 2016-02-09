28c0e2fcfbd59646c339f8469884c121afed2952
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-09 08:22:41 +0100

    Added options to generator constructor

--- Generators/MigrationGenerator.php
+++ Generators/MigrationGenerator.php
@@ -9,37 +9,55 @@ use Way\Generators\Generator;
 class MigrationGenerator implements GeneratorInterface
 {
 
+    /**
+     * @var \Way\Generators\Generator
+     */
     protected $generator;
 
+    /**
+     * @var \Way\Generators\Filesystem\Filesystem
+     */
     protected $filesystem;
 
+    /**
+     * @var \Illuminate\Config\Repository
+     */
     protected $config;
 
+    /**
+     * @var array
+     */
     protected $tables;
 
     /**
-     * MigrationGenerator constructor.
-     * @param $generator
-     * @param $filesystem
-     * @param $config
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @param \Way\Generators\Generator             $generator
+     * @param \Way\Generators\Filesystem\Filesystem $filesystem
+     * @param \Illuminate\Config\Repository         $config
+     * @param array                                 $tables
+     * @param array                                 $options
      */
     public function __construct(
       Generator $generator,
       Filesystem $filesystem,
       $config,
-      $tables
+      $tables,
+      $options
     ) {
         $this->generator = $generator;
         $this->filesystem = $filesystem;
         $this->config = $config;
         $this->tables = $tables ?: [];
+        $this->options = $options;
     }
 
 
     public function execute()
     {
-
-        var_dump("Do something with tables " . implode(', ', $this->tables));
 
 
     }
