efc06d2d52fe7b52b42d682f5c038b1eb37739e8
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-10 15:54:30 +0100

    Added default config for repository generation

--- Config/config.php
+++ Config/config.php
@@ -1,23 +1,28 @@
 <?php
 
 return [
-  'name'      => 'Asgardgenerators',
+  'name'         => 'Asgardgenerators',
 
     // migration
-  'migration' => [
+  'migration'    => [
     'template'    => base_path("Modules/Asgardgenerators/templates") . DIRECTORY_SEPARATOR . "migration.txt",
     'output_path' => database_path("migrations"),
 
   ],
     // models
-  'models'    => [
+  'models'       => [
     'template'    => base_path("Modules/Asgardgenerators/templates") . DIRECTORY_SEPARATOR . "model.txt",
     'output_path' => app_path("Models"),
   ],
+    // repositories
+  'repositories' => [
+    'template'    => base_path("Modules/Asgardgenerators/templates") . DIRECTORY_SEPARATOR . "repository.txt",
+    'output_path' => app_path("Repositories"),
+  ],
     // views
-  'views'     => [
-    'template'    => base_path("Modules/Asgardgenerators/templates/views"),
-    'output_path' => base_path("resource/views"),
+  'views'        => [
+    'template'           => base_path("Modules/Asgardgenerators/templates/views"),
+    'output_path'        => base_path("resource/views"),
     'base_template_name' => 'app',
   ]
 
