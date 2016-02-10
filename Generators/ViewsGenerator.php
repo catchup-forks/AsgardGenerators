15d75c203c260584e137a4331cdf23c0ee722c5e
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-10 11:37:37 +0100

    Added index and show generation functions

--- Generators/ViewsGenerator.php
+++ Generators/ViewsGenerator.php
@@ -16,10 +16,25 @@ class ViewsGenerator extends BaseGenerator implements GeneratorInterface
      */
     public function execute()
     {
+        echo "\nGenerating Views:\n";
+        // create the index view per table
+        foreach ($this->tables->getInfo() as $table => $columns) {
+            // create the base dir for the views
+            $base_dir = $this->getFileGenerationPath() . DIRECTORY_SEPARATOR . "{$table}";
+            if (!file_exists($base_dir)) {
+                mkdir($base_dir);
+            }
 
 
-        echo "boo\n\n\n\n\n\n";
+            foreach ([
+                       'index',
+                       'show'
+                     ] as $item) {
+                $this->generate($table, $columns, $item);
+            }
 
+
+        }
     }
 
     /**
@@ -32,7 +47,7 @@ class ViewsGenerator extends BaseGenerator implements GeneratorInterface
         $path = $this->getOption('templatePath', null);
 
         if (is_null($path)) {
-            $path = config('asgard.asgardgenerators.config.migration.template',
+            $path = config('asgard.asgardgenerators.config.views.template',
               "");
         } else {
             $path .= "views";
@@ -71,4 +86,162 @@ class ViewsGenerator extends BaseGenerator implements GeneratorInterface
 
         return $path;
     }
+
+    /**
+     * Generate the requested view
+     *
+     * @param string $table
+     * @param array  $columns
+     * @param string $name
+     */
+    private function generate($table, $columns = [], $name = "index")
+    {
+
+        $file_to_generate = $this->getFileGenerationPath() . DIRECTORY_SEPARATOR . "{$table}" . DIRECTORY_SEPARATOR . "$name.blade.php";
+
+        if ($this->canGenerate($file_to_generate,
+          $this->getOption('overwrite', false))
+        ) {
+            $this->generator->make(
+              $this->getTemplatePath() . DIRECTORY_SEPARATOR . "$name.txt",
+              $this->createData($table, $columns, $name),
+              $file_to_generate
+            );
+
+            echo "File {$file_to_generate} generated.\n";
+        }
+
+
+    }
+
+
+    /**
+     * @param string $table
+     * @return string
+     */
+    private function createTitleFromTable($table)
+    {
+        $table = ucwords(str_singular($table));
+
+        return str_replace("_", " ", $table);
+    }
+
+    /**
+     * Create the default model name from a given table name
+     *
+     * @param string $table
+     * @return string
+     */
+    private function createDefaultModelNameFromTable($table)
+    {
+        return camel_case(str_singular($table));
+    }
+
+    /**
+     * Determine if a file should be generated, if the overwrite flag is set,
+     * delete the existing file to ensure the regeneration is possible
+     *
+     * @param string $file
+     * @param bool   $overwrite
+     * @return bool
+     */
+    private function canGenerate($file, $overwrite = false)
+    {
+        if (file_exists($file)) {
+            if ($overwrite) {
+                $deleted = unlink($file);
+                if (!$deleted) {
+                    echo "\nFailed to delete existing model $file\n";
+                    return false;
+                }
+            } else {
+                echo "\nSkipped view generation, file already exists. (force using --overwrite) $file\n";
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $table
+     * @param array  $columns
+     * @param string $type
+     * @return array
+     */
+    private function createData($table, $columns = [], $type = 'index')
+    {
+        // base data
+        $model = $this->createDefaultModelNameFromTable($table);
+
+        $data = [
+          'BASE_LAYOUT' => config('asgard.asgardgenerators.config.views.base_template_name',
+            ""),
+          'NAMESPACE'   => $this->getNamespace(),
+          'MODEL'       => $model,
+          'MODELS'      => camel_case($table),
+        ];
+
+        switch ($type) {
+            case 'index':
+                $data += [
+                  'TITLE'         => $this->createTitleFromTable($table),
+                  'TABLE_HEADERS' => $this->createIndexTableHeaderData($table,
+                    $columns),
+                  'TABLE_CONTENT' => $this->createIndexTableContentData($table,
+                    $columns),
+                  'BASE_ROUTE'    => "admin.$model.$model",
+                ];
+                break;
+            case 'show':
+                $data += [
+                  'TITLE' => 'id'
+                ];
+                break;
+        }
+
+
+        return $data;
+    }
+
+    /**
+     * Create the table header for the index view
+     *
+     * @param string $table
+     * @param array  $columns
+     * @return string
+     */
+    private function createIndexTableHeaderData($table, $columns)
+    {
+        $titles = [];
+
+        foreach (array_keys($columns['columns']) as $column) {
+            $titles[] = "<th>$column</th>";
+        }
+
+        return implode("\n", $titles);
+    }
+
+    /**
+     * Create the table content for the index view
+     *
+     * @param string $table
+     * @param array  $columns
+     * @return string
+     */
+    private function createIndexTableContentData($table, $columns)
+    {
+        $line = [];
+
+        // determine the model name
+        $model = $this->createDefaultModelNameFromTable($table);
+
+        foreach ($columns['columns'] as $column => $datatype) {
+            $line[] = "<td>{{ \${$model}->{$column} }}</td>";
+        }
+
+        return implode("\n", $line);
+    }
+
+
 }
