8d659d565f559f4989f1caf39ca509717376e291
Stijn Tilleman <tilleman.stijn@gmail.com>
2016-02-17 11:59:09 +0100

    Fixed select and select-multiple default value(s)

--- Resources/views/partials/fields/select-multiple.blade.php
+++ Resources/views/partials/fields/select-multiple.blade.php
@@ -1,11 +1,11 @@
-
 <p>
     <label for="">{{ $title }}</label>
 
     <select class="form-control multiselect" multiple="multiple" name="{{ $name }}[]">
-        @foreach($options->pluck($primary_key) as $optionKey => $option)
-            <option value="{{ $optionKey }}" @if(array_key_exists($selected, $options))selected="selected"@endif>{{ $option }}</option>
+        @foreach($options->pluck($primary_key, $primary_key) as $optionKey => $option)
+            <option value="{{ $optionKey }}"
+                    @if(in_array($optionKey, $selected))selected="selected"@endif
+            >{{ $option }}</option>
         @endforeach
     </select>
-
 </p>
