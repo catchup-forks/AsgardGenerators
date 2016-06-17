841f0ca2b2f1899e43bc96ad46bc7315cb4370e9
Pepijn Olivier <olivier.pepijn@gmail.com>
2016-06-17 17:29:09 +0200

    Update textarea.blade.php

--- Resources/views/partials/fields/textarea.blade.php
+++ Resources/views/partials/fields/textarea.blade.php
@@ -1,14 +1,15 @@
 <p>
+    <?php $classes = isset($classes) ? $classes : 'form-control'; ?>
     @if(isset($is_translation) && $is_translation === 1)
         {!! Form::label("{$lang}[{$name}]", $title) !!}
         {!! Form::textarea("{$lang}[{$name}]",
             Input::old("{$lang}[{$name}]", isset($value[$lang])?$value[$lang]:'') ,
-            ['class' => 'form-control', 'placeholder' => $placeholder ]
+            ['class' => $classes, 'placeholder' => $placeholder ]
             ) !!}
     @else
         {!! Form::label($name, $title) !!}
         {!! Form::textarea($name, $value, [
-            'class' => 'form-control',
+            'class' => $classes,
             'placeholder' => $placeholder,
         ]) !!}
     @endif
